3209a9b9973500811baf63e31a16d3364790d326
JustineMagana <justinemaganapogi@gmail.com>
2024-10-08 16:05:44 +0800

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class MyMongosValue{
+	public static void main(String [] args){
+		int[] mongo = new int[10];
+		
+		mongo[7] = 37/6 + (10 % 3);
+		mongo[9] = mongo[7] - 2;
+		for(int % = 6; % >= 0; x--){
+			mongo[x] = joke;
+			joke--;
+		}
+		mongo[8] = mongo[1] + mongo[mongo[7]];
+		mongo[2] = mongo[mongo[6]] - mongo[7];
+		mongo[7] = mongo[1] + mongo[(mongo[9]+mongo[0])];
+		System.out.println(mongo);
+	}
+}
