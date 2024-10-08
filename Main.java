cf6c7534bab66a121378a562448f73397857f1ec
JustineMagana <justinemaganapogi@gmail.com>
2024-10-08 15:51:53 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -8,13 +8,3 @@ class Main {
         halogen.turnOff();
     }
 }
-
-class Lamp {
-    public void turnOn() {
-        System.out.println("The lamp is on.");
-    }
-    
-    public void turnOff() {
-        System.out.println("The lamp is off.");
-    }
-}
